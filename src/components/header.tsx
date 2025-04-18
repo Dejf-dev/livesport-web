b3e6a576b0a20cd93f9d0b937b32286dba30ccb5
David Ratimec <ratimec.david99@gmail.com>
2025-04-18 21:37:03 +0200

    add header

---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import {FC} from "react";
+import Image from "next/image";
+import * as React from "react";
+import {usePathname} from "next/navigation";
+import {Separator} from "@/components/ui/separator";
+import Link from "next/link";
+
+export const Header: FC = () => {
+    const pathname = usePathname();
+
+    const makeTitle = () => {
+        if (pathname == "/") {
+            return "Výsledky"
+        }
+
+        return pathname.split("/").filter(Boolean).map(part => part.charAt(0).toUpperCase() + part.slice(1));
+    }
+
+    return (
+        <div className="bg-header-background mb-7">
+            <div className="w-full flex items-center relative my-3">
+                <Link className="h-full flex items-center" href="/">
+                    <Image src="/livesport-logo.jpg" alt="Livesport logo" width={100} height={100}/>
+                    <h1 className="text-6xl">Livesport</h1>
+                </Link>
+                <h2 className="text-5xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">{makeTitle()}</h2>
+            </div>
+            <Separator />
+        </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {Header} from "@/components/header";
+import {Poppins} from "next/font/google";
+
+const oswald = Poppins({
+    weight: "400",
+    subsets: ["latin"]
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <div className={oswald.className}>
+        <Header></Header>
+        <Component {...pageProps} />
+      </div>
+  );
 }
