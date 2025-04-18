e55a4abf0b19bc97900d1c92f3cfc9f36a14f34c
David Ratimec <ratimec.david99@gmail.com>
2025-04-18 23:05:52 +0200

    add constants used for API URL

---
+++ src/constants/QueryParamNames.ts
@@ -0,0 +1,11 @@
+export const LANG_ID = "lang-id"
+
+export const PROJECT_ID = "project-id"
+
+export const PROJECT_TYPE_ID = "project-type-id"
+
+export const SPORT_IDS = "sport-ids"
+
+export const TYPE_IDS = "type-ids"
+
+export const QUERY = "q"
