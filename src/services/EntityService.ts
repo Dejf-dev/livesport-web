80f540f3752b144043f25a49de746d2e635b47ec
David Ratimec <ratimec.david99@gmail.com>
2025-04-18 23:06:43 +0200

    add service for fetching data

---
+++ src/services/EntityService.ts
@@ -0,0 +1,28 @@
+import {
+    LANG_ID,
+    PROJECT_ID,
+    PROJECT_TYPE_ID,
+    SPORT_IDS,
+    TYPE_IDS,
+    QUERY
+} from "@/constants/QueryParamNames"
+import axios, {AxiosResponse} from "axios";
+
+export default class EntityService {
+    private urlMandQueryParams = `${process.env.NEXT_PUBLIC_API_URL}?` +
+                                        `${LANG_ID}=${process.env.NEXT_PUBLIC_LANG_ID}&` +
+                                        `${PROJECT_ID}=${process.env.NEXT_PUBLIC_PROJECT_ID}&` +
+                                        `${PROJECT_TYPE_ID}=${process.env.NEXT_PUBLIC_PROJECT_TYPE_ID}`
+
+    public fetchData = async (sportIds: number[], typeIds: number[], q: string): Promise<AxiosResponse> => {
+        const sportIdsQueryParam = sportIds.join(",")
+        const typeIdsQueryParam = typeIds.join(",")
+
+        const finalUrl = `${this.urlMandQueryParams}&` +
+                                `${SPORT_IDS}=${sportIdsQueryParam}&` +
+                                `${TYPE_IDS}=${typeIdsQueryParam}&` +
+                                `${QUERY}=${q}`
+
+        return await axios.get(`${finalUrl}`);
+    }
+}
