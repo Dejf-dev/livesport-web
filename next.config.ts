c552b7e8e48cb56e0c19b65807538a8779ab64e6
David Ratimec <ratimec.david99@gmail.com>
2025-04-19 15:31:04 +0200

    add placeholders

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://www.livesport.cz/res/image/data/**")]
+  }
 };
 
 export default nextConfig;
